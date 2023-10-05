80ad59af31545bfa187dcb797e552c2ead028b00
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-10-05 19:16:03 -0300

    set navigation

--- App.js
+++ App.js
@@ -1,37 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ScrollView, Image, TextInput, Button } from 'react-native';
+import AppNavigation from './src/navigation';
 
 export default function App() {
-  const accion = ()=>{
-    Alert.alert("Miau", "Hello Kitty uwu")
-  }
+  
   return (
-    <ScrollView style={styles.container}>
-      <Text>hello world, im lu</Text>
-      {/* <StatusBar style="auto" /> */}
-      <View>
-        <Text>This is a kitty</Text>
-        <Image source={{
-            uri: 'https://media.giphy.com/media/QoxjcvbhMRwROVGCK4/giphy.gif',
-          }}
-          style={{width: 200, height: 200}}/>
-          <Button title="ejecuta el hola mundo" onPress={accion}/>
-      </View>
-      <TextInput
-        style={{
-          height: 40,
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-        defaultValue="escribi algo uwu"
-      />
-
-    </ScrollView>
-  );
+    <AppNavigation/>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-});
