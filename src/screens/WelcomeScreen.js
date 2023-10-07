78c86044067c0f597ca083fbc4e9c84af14068c1
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-10-06 22:43:53 -0300

    add animation in WelcomeScreen

--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,20 +1,32 @@
 import { View, Text} from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen"
-
+import Animated,{ useSharedValue, withSpring } from "react-native-reanimated"
+import { useEffect } from "react"
 
 export default function WelcomeScreen() {
+
+    const ring1padding = useSharedValue(0)
+    const ring2padding = useSharedValue(0)
+
+    useEffect(() => {
+        ring1padding.value = 0
+        ring2padding.value = 0
+        setTimeout(() => ring1padding.value = withSpring(ring1padding.value+hp(5)),100) 
+        setTimeout(() => ring2padding.value = withSpring(ring1padding.value+hp(5.5)),300) 
+    })
+
     return(
         <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
             <StatusBar style="light"/>
 
             {/* logo image with rings */}
-            <View className="bg-white/20 rounded-full" style={{padding:hp(5.5)}}>
-                <View className="bg-white/20 rounded-full" style={{padding:hp(5)}}>
+            <Animated.View className="bg-white/20 rounded-full" style={{padding:ring2padding}}>
+                <Animated.View className="bg-white/20 rounded-full" style={{padding:ring1padding}}>
                     <Image source={require('../../assets/images/welcome.png')} 
                            style={{width: hp(20), height: hp(20)}}/>
-                </View>
-            </View>
+                </Animated.View>
+            </Animated.View>
             
             {/* title and punchline */}
             <View className="flex items-center space-y-2">
