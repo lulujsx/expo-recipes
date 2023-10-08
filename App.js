fa08cb359bf80b0011241d0eab03cd47bebb14a3
Luana Vallejos <luanalorenavallejos@gmail.com>
2023-10-08 00:33:07 -0300

    trying things

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ScrollView, Image, TextInput, Button } from 'react-native';
 import AppNavigation from './src/navigation';
 
 export default function App() {
--- src/screens/WelcomeScreen.js
+++ src/screens/WelcomeScreen.js
@@ -1,24 +1,24 @@
 import { View, Text, Image} from "react-native"
 import { StatusBar } from "expo-status-bar"
 import { widthPercentageToDP as wp, heightPercentageToDP as hp } from "react-native-responsive-screen"
-import Animated,{ useSharedValue, withSpring } from "react-native-reanimated"
+// import Animated,{ useSharedValue, withSpring } from "react-native-reanimated"
 import { useEffect } from "react"
 import { useNavigation } from "@react-navigation/native"
 
 export default function WelcomeScreen() {
 
-    const ring1padding = useSharedValue(0)
-    const ring2padding = useSharedValue(0)
+    // const ring1padding = useSharedValue(0)
+    // const ring2padding = useSharedValue(0)
 
-    const navigation = useNavigation()
+    // const navigation = useNavigation()
 
     useEffect(() => {
-        ring1padding.value = 0
-        ring2padding.value = 0
-        setTimeout(() => ring1padding.value = withSpring(ring1padding.value+hp(5)),100) 
-        setTimeout(() => ring2padding.value = withSpring(ring2padding.value+hp(5.5)),300) 
+        // ring1padding.value = 0
+        // ring2padding.value = 0
+        // setTimeout(() => ring1padding.value = withSpring(ring1padding.value+hp(5)),100) 
+        // setTimeout(() => ring2padding.value = withSpring(ring2padding.value+hp(5.5)),300) 
 
-        setTimeout(()=> navigation.navigate('Home'),2500)
+        // setTimeout(()=> navigation.navigate('Home'),2500)
     },[])
 
     return(
@@ -47,8 +47,8 @@ export default function WelcomeScreen() {
 
             {/* logo image with rings */}
 
-            <View className="bg-white/20 rounded-full" style={{padding:ring2padding}}>
-                <View className="bg-white/20 rounded-full" style={{padding:ring1padding}}>
+            <View className="bg-white/20 rounded-full" style={{padding:4}}>
+                <View className="bg-white/20 rounded-full" style={{padding:2}}>
                     <Image source={require('../../assets/images/welcome.png')} 
                            style={{width: hp(20), height: hp(20)}}/>
                 </View>
